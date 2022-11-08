877cedafaceb456682aa986ef873410ba065ab50
LimJieLe <JIELE.21@ichat.sp.edu.sg>
2022-11-08 10:54:29 +0800

    Lab4

--- src/PiLedTest.py
+++ src/PiLedTest.py
@@ -11,14 +11,8 @@ from time import sleep
 
 from hal import hal_led as led
 from hal import hal_input_switch as switch
-from hal import hal_lcd as LCD
-from hal import hal_dc_motor as dc_motor
-from hal import hal_buzzer as buzzer
-from hal import hal_servo as servo
-import version as ver
 
 def blink_led(delay):
-    # Led Blink
     led.init()
 
     led.set_output(0, 1)
@@ -34,16 +28,12 @@ def blink_led(delay):
     time.sleep(delay)
 
 def main():
-    lcd = LCD.lcd()
-    if switch.read_slide_switch() == 1 :
-        sleep(0.5)
-        lcd.backlight(0)  # turn backlight off
-
-        sleep(0.5)
-        lcd.backlight(1)  # turn backlight on
+    switch.init()
+    x = switch.read_slide_switch()
+    if x == 1 :
+        blink_led(200)
     else :
-        sleep(0.5)
-        lcd.backlight(0)  # turn backlight on
+        led.set_output(0, 0)
 
 if __name__ == "__main__":
     main()
